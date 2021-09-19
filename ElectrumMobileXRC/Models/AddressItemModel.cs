f9b778a1c9e735b60887ccaf870af6d884cf16f8
Marcus0x <marcus0x@xrhodium.org>
2021-09-19 11:15:37 +0200

    Added addresses list box with copy to clipboard feature

---
+++ ElectrumMobileXRC/Models/AddressItemModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectrumMobileXRC.Models
+{
+    public class AddressItemModel
+    {
+        public string Address { get; set; }
+        public double Balance { get; set; }
+    }
+}
--- ElectrumMobileXRC/PageModels/AddressesPageModel.cs
+++ ElectrumMobileXRC/PageModels/AddressesPageModel.cs
@@ -1,12 +1,78 @@
-﻿using FreshMvvm;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using FreshMvvm;
+using ElectrumMobileXRC.Models;
+using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace ElectrumMobileXRC.PageModels
 {
     public class AddressesPageModel : FreshBasePageModel
     {
+        private ObservableCollection<AddressItemModel> _addresses = new ObservableCollection<AddressItemModel>();
+        public ObservableCollection<AddressItemModel> Addresses
+        {
+            get
+            {
+                return _addresses;
+            }
+            set
+            {
+                _addresses = value;
+            }
+        }
+
+        public ICommand BackButtonCommand { get; set; }
+        public ICommand MenuButtonCommand { get; set; }
+        public ICommand CopyButtonCommand { get; set; }
+
         public AddressesPageModel()
         {
+            var addItem = new AddressItemModel();
+            addItem.Balance = 2;
+            addItem.Address = "TX7WVvmDtvMrx293ksM8io9Y15hdZUPPTq";
+            Addresses.Add(addItem);
 
+            addItem = new AddressItemModel();
+            addItem.Balance = 200.00001;
+            addItem.Address = "TGu88zPeBZ3A6E5ESvQPkiX5Hyn5oJhUtG";
+            Addresses.Add(addItem);
+
+
+            BackButtonCommand = new Command(async () =>
+            {
+                await CoreMethods.PushPageModel<MainPageModel>();
+            });
+
+            MenuButtonCommand = new Command(async () =>
+            {
+                var actionSheet = await CoreMethods.DisplayActionSheet("Electrum Mobile XRC", "Back", null, "Addresses", "Network");
+
+                switch (actionSheet)
+                {
+                    case "Addresses":
+                        await CoreMethods.PushPageModel<AddressesPageModel>();
+
+                        break;
+
+                    case "Network":
+                        await CoreMethods.PushPageModel<NetworkPageModel>();
+
+                        break;
+                }
+            });
+
+            CopyButtonCommand = new Command(async (value) =>
+            {
+                await Clipboard.SetTextAsync((string)value);
+                if (Clipboard.HasText)
+                {
+                    var text = await Clipboard.GetTextAsync();
+
+                    await CoreMethods.DisplayAlert("Success", string.Format("Your copied address is ({0})", (string)value), "OK");
+                }
+            });
         }
     }
 }
