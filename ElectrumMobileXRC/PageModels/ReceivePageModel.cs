30195cbfbecca39293c59e86868b20342545c06b
Marcus0x <marcus0x@xrhodium.org>
2021-09-19 11:34:14 +0200

    Added menu buttons to ReceivePage

--- ElectrumMobileXRC/PageModels/ReceivePageModel.cs
+++ ElectrumMobileXRC/PageModels/ReceivePageModel.cs
@@ -2,11 +2,15 @@
 using Xamarin.Forms;
 using QRCoder;
 using System.IO;
+using System.Windows.Input;
 
 namespace ElectrumMobileXRC.PageModels
 {
     public class ReceivePageModel : FreshBasePageModel
     {
+        public ICommand BackButtonCommand { get; set; }
+        public ICommand MenuButtonCommand { get; set; }
+
         public ImageSource QrCodeImage { get; set; }
 
         public ReceivePageModel()
@@ -16,6 +20,29 @@ namespace ElectrumMobileXRC.PageModels
             PngByteQRCode qRCode = new PngByteQRCode(qrCodeData);
             byte[] qrCodeBytes = qRCode.GetGraphic(20);
             QrCodeImage = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
+
+            BackButtonCommand = new Command(async () =>
+            {
+                await CoreMethods.PushPageModel<MainPageModel>();
+            });
+
+            MenuButtonCommand = new Command(async () =>
+            {
+                var actionSheet = await CoreMethods.DisplayActionSheet("Electrum Mobile XRC", "Back", null, "Addresses", "Network");
+
+                switch (actionSheet)
+                {
+                    case "Addresses":
+                        await CoreMethods.PushPageModel<AddressesPageModel>();
+
+                        break;
+
+                    case "Network":
+                        await CoreMethods.PushPageModel<NetworkPageModel>();
+
+                        break;
+                }
+            });
         }
     }
 }
